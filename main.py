6b7022847216d1400daa3e1a77aab448c46bc482
krizhnaa <krizhnaprem@gmail.com>
2023-12-28 16:24:01 +0530

    Created a canvas with GUI Logo with Tkinter

--- main.py
+++ main.py
@@ -1,5 +1,21 @@
+from tkinter import *
+
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
 # ---------------------------- UI SETUP ------------------------------- #
+
+window = Tk()
+# window.minsize(width=200, height=200)
+window.config(padx=20, pady=20)
+window.title("Password Manager")
+
+canvas = Canvas(height=200, width=200)
+gui_img = PhotoImage(file="logo.png")
+canvas.create_image(100, 100, image=gui_img)
+canvas.grid(row=0, column=3)
+
+
+
+window.mainloop()
